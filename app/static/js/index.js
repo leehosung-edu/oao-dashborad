a9ebac5f8748abcc1efed1aea01567355d5cf0cd
leehosung <ka45416417@gmail.com>
2025-05-05 11:54:58 +0900

    위원회 정보 삽입

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -12,7 +12,10 @@ fetch('/static/html/Comm.html')  // header.html을 불러옴
     .then(response => response.text())  // 응답을 텍스트로 변환
     .then(data => document.getElementById('Comm').innerHTML = data)  // 내용 삽입
     .catch(error => console.error('Comm로딩 실패:', error));  // 오류 처리 
-
+fetch('/static/html/committe_info.html')  // header.html을 불러옴
+    .then(response => response.text())  // 응답을 텍스트로 변환
+    .then(data => document.getElementById('committe_info').innerHTML = data)  // 내용 삽입
+    .catch(error => console.error('committe_info로딩 실패:', error));  // 오류 처리
     function showPopup() {
         document.getElementById('popup').style.display = 'block';
         document.getElementById('overlay').style.display = 'block';
