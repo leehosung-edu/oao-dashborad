d9e1c380e9255d32971f960b99025293be3918aa
leehosung <ka45416417@gmail.com>
2025-05-06 16:09:31 +0900

    차트 프로토타입, 일단 내 능력은 여기가 한계임

---
+++ app/static/js/htmlfetch.js
@@ -0,0 +1,18 @@
+fetch('/static/html/Rec_Comm.html')  // header.html을 불러옴
+    .then(response => response.text())  // 응답을 텍스트로 변환
+    .then(data => document.getElementById('Rec_Comm').innerHTML = data)  // 내용 삽입
+    .catch(error => console.error('Rec_Comm 로딩 실패:', error));  // 오류 처리
+
+fetch('/static/html/Rec_Comm_Upper.html')  // header.html을 불러옴
+    .then(response => response.text())  // 응답을 텍스트로 변환
+    .then(data => document.getElementById('Rec_Comm_Upper').innerHTML = data)  // 내용 삽입
+    .catch(error => console.error('Rec_Comm_Upper 로딩 실패:', error));  // 오류 처리
+
+fetch('/static/html/Comm.html')  // header.html을 불러옴
+    .then(response => response.text())  // 응답을 텍스트로 변환
+    .then(data => document.getElementById('Comm').innerHTML = data)  // 내용 삽입
+    .catch(error => console.error('Comm로딩 실패:', error));  // 오류 처리 
+fetch('/static/html/committe_info.html')  // header.html을 불러옴
+    .then(response => response.text())  // 응답을 텍스트로 변환
+    .then(data => document.getElementById('committe_info').innerHTML = data)  // 내용 삽입
+    .catch(error => console.error('committe_info로딩 실패:', error));  // 오류 처리
