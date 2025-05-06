a599504d6524da117e8e57ec270a0f2f9657d58f
leehosung <ka45416417@gmail.com>
2025-05-06 16:42:40 +0900

    차트 js로 조작 완료!!

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -11,6 +11,11 @@
 
 
 
-const chart=document.getElementById("chart");
-
+    window.onload = function() {
+        let chart = document.getElementById("chart");
+        function chart_change(color_1st,ratio_1st,color_2nd,ratio_2nd,color_3rd,ratio_3rd,chart){   
+            chart.style.background="conic-gradient("+color_1st+" 0%"+ratio_1st+"%, "+color_2nd+" "+ratio_1st+"% "+(ratio_1st+ratio_2nd)+"%, "+color_3rd+" "+(ratio_1st+ratio_2nd)+"% "+(ratio_1st+ratio_2nd+ratio_3rd)+"%)"
+        }
+        chart_change("red",30,"blue",30,"green",20,chart);}
+      
   
