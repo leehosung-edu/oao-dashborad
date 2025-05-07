6fd28af14d5481b370929d3d9ccb140bfe96a52e
leehosung <ka45416417@gmail.com>
2025-05-07 10:26:55 +0900

    새 사이트 이동 추가함, 먼가 이상함

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -8,14 +8,10 @@
         document.getElementById('popup').style.display = 'none';
         document.getElementById('overlay').style.display = 'none';
     }
-
-
-
-    window.onload = function() {
-        let chart = document.getElementById("chart");
-        function chart_change(color_1st,ratio_1st,color_2nd,ratio_2nd,color_3rd,ratio_3rd,chart){   
-            chart.style.background="conic-gradient("+color_1st+" 0%"+ratio_1st+"%, "+color_2nd+" "+ratio_1st+"% "+(ratio_1st+ratio_2nd)+"%, "+color_3rd+" "+(ratio_1st+ratio_2nd)+"% "+(ratio_1st+ratio_2nd+ratio_3rd)+"%)"
-        }
-        chart_change("#39CEF3",30,"blue",30,"#72CA3D",20,chart);}
-      
-  
+    
+    function click_committe_info(title) {
+        var newWindow = window.open("/static/html/committe_info.html", "_blank");
+        newWindow.onload = function() {
+            newWindow.document.title = title;
+        };
+    }
