2861fc9ecd6a573a4ac1fa17059ef09ef22960b2
leehosung <ka45416417@gmail.com>
2025-05-07 10:38:30 +0900

    [fix]새 사이트 이동 개선, 캐시 무시하는 코드를 index.js에 추가함, url에 현재 시간을 넣는 방식임, 수정 필요

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -10,7 +10,8 @@
     }
     
     function click_committe_info(title) {
-        var newWindow = window.open("/static/html/committe_info.html", "_blank");
+        const cachelees_url="/static/html/committe_info.html"+"?nocache"+ new Date().getTime()//캐시를 무시하는 코드, 개선 필요
+        var newWindow = window.open(cachelees_url,"_blank");
         newWindow.onload = function() {
             newWindow.document.title = title;
         };
