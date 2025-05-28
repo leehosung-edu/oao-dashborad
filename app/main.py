52a12204a748aa22a6546300179d415c135241db
Hyunbin Kim <taque88@naver.com>
2025-05-28 14:01:17 +0900

    [FIX] 임포트 경로 수정

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 from dotenv import load_dotenv
 from pathlib import Path
 from typing import List
-from .api.endpoints import schedule
+from app.api.endpoints import schedule
 import os
 
 
